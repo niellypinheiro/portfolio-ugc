bfee306a4e311c1f3dae9c66379a8459bf7943c6
niellypinheiro <niellypinheirougccreator@gmail.com>
2026-09-25 17:06:55 -0300

    Add files via upload

--- js/banco.js
+++ js/banco.js
@@ -18,7 +18,7 @@
     chave: CHAVE_PUBLICA,
     emailDaDona: "niellypinheirougccreator@gmail.com",
     /* endereço da pasta do site (funciona no GitHub Pages e no computador) */
-    raiz: "https://niellypinheiro.github.io/portfolio-ugc/"
+    raiz: "https://niellypinheiro.com.br/"
   };
   /* Endereço do editor de SQL do seu projeto no Supabase (o painel usa nos avisos de "falta rodar o banco.sql") */
   window.BANCO.editorSql = "https://supabase.com/dashboard/project/" + URL_DO_PROJETO.replace("https://", "").split(".")[0] + "/sql/new";
